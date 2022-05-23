29be051410ce74a48168cd16867141a7bbf90205
turalismylv <turalhi@code.edu.az>
2022-05-24 01:03:32 +0400

    HomeWork2405

---
+++ ConsoleApp1/ConsoleApp1/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Person
+    {
+        public string Name;
+        public string Surname;
+        public int Age;
+        public bool IsAdult;
+        public Person(string name, string surname, int age)
+        {
+            Name = name;
+            Surname = surname;
+            Age = age;
+            if (Age > 18)
+            {
+                IsAdult = true;
+            }
+            else
+            {
+                IsAdult = false;
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //            Person classı olsun
+            //Name, Surname, Age, İsAdult propertyləri olsun.
+            //                Və name, surname, age parametrlərini qəbul edən Constructoru olsun
+            //                .Obyekt yarananda əgər yaşı 18dən böyükdürsə İsAdult - ı true əks halda false olsun
+
+            Person prsn = new Person("Tural","Ismayilov",20);
+            Console.WriteLine($"{prsn.Name} {prsn.Surname} {prsn.Age} {prsn.IsAdult}");
+        }
+    }
+}
